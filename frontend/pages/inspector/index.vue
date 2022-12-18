1970dd9e4f0592ddd60e9909c9760283f7f46ce1
butschster <butschster@gmail.com>
2022-12-18 20:05:36 +0400

    Adds missed files

---
+++ frontend/pages/inspector/index.vue
@@ -0,0 +1,15 @@
+<script>
+import Event from "@/Components/Events/Inspector/Event"
+import EventsPage from "@/mixins/EventsPage"
+
+export default {
+  components: {Event},
+  extends: EventsPage,
+  data() {
+    return {
+      title: 'Inspector events',
+      type: 'inspector',
+    }
+  },
+}
+</script>
