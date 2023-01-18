fdf4096c220fa378458a25a88b7258e80fc07039
Kuchuk Andrey <kreezag@gmail.com>
2023-01-18 20:52:11 +0400

    add event-header component

---
+++ new-frontend/components/EventHeader/EventHeader.stories.ts
@@ -0,0 +1,46 @@
+import { action } from '@storybook/addon-actions'
+import EventHeader from './EventHeader.vue';
+import { EVENT_TYPES } from '~/config/constants';
+
+export default {
+  title: "Event/EventHeader",
+  component: EventHeader,
+  argTypes: {
+    eventType: {
+      control: { type: 'select' },
+      options: Object.values(EVENT_TYPES),
+      mapping: EVENT_TYPES
+    },
+  }
+};
+
+const Template = (args: typeof Object) => ({
+  components: { EventHeader },
+  methods: {
+    action
+  },
+  setup() {
+    return {
+      args,
+    };
+  },
+  template: `
+    <event-header
+      v-bind="args"
+      @delete="(a) => action('Delete event')(a)"
+      @toggle-view="(a) => action('Toggle event')(a)"
+      @copy="(a) => action('Copied event')(a)"
+      @download="(a) => action('Downloaded event')(a)"
+    />
+`,
+});
+
+export const Default = Template.bind({});
+Default.args = {
+  eventUrl: 'https://github.com/buggregator/spiral-app',
+  eventType: EVENT_TYPES.SENTRY,
+  isOpen: true,
+  isVisibleControls: true,
+  eventId: 'test-event-id',
+  tags: ['one', 'two', 'tree']
+};
