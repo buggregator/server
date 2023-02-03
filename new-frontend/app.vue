a0aad8797465496c3df9e88a2b4f3c76524e1005
Kuchuk Andrey <kreezag@gmail.com>
2023-02-03 17:29:22 +0400

    add default layout

--- new-frontend/app.vue
+++ new-frontend/app.vue
@@ -1,18 +1,5 @@
 <template>
-  <div style="padding: 20px">
-    <event-header
-      event-url="https://storybook.js.org/docs/vue/writing-stories/args"
-      event-type="Sentry"
-      :tags="['one', 'two', 'tree']"
-      :is-open="true"
-      event-id="test-id"
-    />
-    &lt;! -- body --&gt;
-    <event-footer
-      :origin-config="{ key1: 'value1', key2: 'value2' }"
-      server-name="Best server"
-    />
-  </div>
+  <NuxtPage />
 </template>
 
 <script lang="ts" setup>
---
+++ new-frontend/layouts/default.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="main-layout">
+    <div class="main-layout__sidebar-wrap">
+      <layout-sidebar :is-connected="isConnected" />
+    </div>
+
+    <div class="main-layout__content">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import LayoutSidebar from "~/components/LayoutSidebar/LayoutSidebar.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {
+    LayoutSidebar,
+  },
+  computed: {
+    isConnected() {
+      // return this.$store.getters['ws/connected']
+      return true;
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.main-layout {
+  @apply flex h-screen items-stretch;
+}
+
+.main-layout__sidebar-wrap {
+  @apply w-10 md:w-14 lg:w-16 flex-none border-r;
+}
+
+.main-layout__content {
+  @apply flex flex-col h-full flex-1;
+
+  & > div {
+    @apply flex flex-col h-full flex-1;
+  }
+}
+
+.main-layout__sidebar {
+  @apply w-10 md:w-14 lg:w-16 fixed h-screen;
+}
+</style>
