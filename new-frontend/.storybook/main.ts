4fc4d8e3e7c081d620143b03f1e0a1f8153a5625
Kuchuk Andrey <kreezag@gmail.com>
2023-02-24 20:41:02 +0400

    update storybook config

--- new-frontend/.storybook/main.ts
+++ new-frontend/.storybook/main.ts
@@ -7,6 +7,8 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../components/**/*.stories.@(js|jsx|ts|tsx)",
+    "../layouts/**/*.stories.@(js|jsx|ts|tsx)",
+    "../pages/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
