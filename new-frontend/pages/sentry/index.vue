70ebef29fbb40ace7592b2c287f97724e30130df
Kuchuk Andrey <kreezag@gmail.com>
2023-03-05 23:58:32 +0400

    add title to sentry events page

--- new-frontend/pages/sentry/index.vue
+++ new-frontend/pages/sentry/index.vue
@@ -25,5 +25,10 @@ export default defineComponent({
       clearEvents: () => {},
     };
   },
+  head() {
+    return {
+      title: `Sentry [${this.events.length}] | Buggregator`,
+    };
+  },
 });
 </script>
