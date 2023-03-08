5b46f27c560a8a17a0de3822d9be2de5946642d6
butschster <butschster@gmail.com>
2023-03-08 12:31:54 +0400

    Fixes SentryFrame component name

--- new-frontend/components/EventSentry/SentryFrame.stories.ts
+++ new-frontend/components/EventSentry/SentryFrame.stories.ts
@@ -1,11 +1,11 @@
 import { Meta, Story } from "@storybook/vue3";
 import { normalizeSentryEvent } from "~/utils/normalize-event";
-import EventSentryFrame, { SentryFrame } from '~/components/EventSentryFrame/EventSentryFrame.vue';
 import sentryEventMock from '~/mocks/sentry.json'
 import { Sentry } from "~/config/types";
+import EventSentryFrame, { SentryFrame } from './SentryFrame.vue';
 
 export default {
-  title: "Event/EventSentryFrame",
+  title: "Sentry",
   component: EventSentryFrame
 } as Meta<typeof EventSentryFrame>;
 
@@ -19,9 +19,9 @@ const Template: Story = (args) => ({
   template: `<event-sentry-frame v-bind="args" />`,
 });
 
-export const Default = Template.bind({});
+export const Frame = Template.bind({});
 
-Default.args = {
+Frame.args = {
   isOpen: true,
   frame: (normalizeSentryEvent(sentryEventMock)?.payload as Sentry)?.exception?.values[0]?.stacktrace?.frames[1] as SentryFrame,
 };
