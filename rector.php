18f8b1e0db90ff2d041a436d0b86ee6f7742957e
butschster <butschster@gmail.com>
2024-04-30 10:20:38 +0400

    Fixes phpunit

--- rector.php
+++ rector.php
@@ -21,6 +21,7 @@ return RectorConfig::configure()
         __DIR__ . '/app',
         __DIR__ . '/tests',
     ])
+    ->withCache(cacheDirectory: __DIR__ . '.cache/rector')
     // uncomment to reach your current PHP version
     // ->withPhpSets()
     ->withRules([
