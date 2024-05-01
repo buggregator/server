46089e53699f8c563e23fe193ffdf4634c8fda9e
butschster <butschster@gmail.com>
2024-05-01 23:33:20 +0400

    Fix

--- bin/get-binaries.sh
+++ bin/get-binaries.sh
@@ -1,8 +1,8 @@
 echo "Download centrifugo"
-#wget --timeout=10 https://github.com/centrifugal/centrifugo/releases/download/v4.0.3/centrifugo_4.0.3_linux_amd64.tar.gz
-#tar xvfz centrifugo_4.0.3_linux_amd64.tar.gz centrifugo
-#rm -rf centrifugo_4.0.3_linux_amd64.tar.gz
-#chmod +x centrifugo
+wget --timeout=10 https://github.com/centrifugal/centrifugo/releases/download/v4.0.3/centrifugo_4.0.3_linux_amd64.tar.gz
+tar xvfz centrifugo_4.0.3_linux_amd64.tar.gz centrifugo
+rm -rf centrifugo_4.0.3_linux_amd64.tar.gz
+chmod +x centrifugo
 
 echo "Download dolt db"
 wget --timeout=10 https://github.com/dolthub/dolt/releases/download/v1.35.12/dolt-linux-amd64.tar.gz
