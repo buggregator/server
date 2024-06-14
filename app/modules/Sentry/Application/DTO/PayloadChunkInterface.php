ddc7262add3316f9e755e371b7787ab63f1d817f
butschster <butschster@gmail.com>
2024-06-14 12:45:27 +0400

    Cs fix

--- app/modules/Sentry/Application/DTO/PayloadChunkInterface.php
+++ app/modules/Sentry/Application/DTO/PayloadChunkInterface.php
@@ -4,7 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\Sentry\Application\DTO;
 
-interface PayloadChunkInterface extends \JsonSerializable, \Stringable
-{
-
-}
+interface PayloadChunkInterface extends \JsonSerializable, \Stringable {}
