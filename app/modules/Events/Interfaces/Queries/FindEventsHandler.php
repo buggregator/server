a64f22d06797eacdd08e9d19aad8085c073aa79e
butschster <butschster@gmail.com>
2024-07-28 19:06:26 +0400

    When events requested without project, events for all projects should be returned

--- app/modules/Events/Interfaces/Queries/FindEventsHandler.php
+++ app/modules/Events/Interfaces/Queries/FindEventsHandler.php
@@ -6,19 +6,24 @@ namespace Modules\Events\Interfaces\Queries;
 
 use App\Application\Commands\FindEvents;
 use Modules\Events\Domain\EventRepositoryInterface;
+use Spiral\Core\Container;
 use Spiral\Cqrs\Attribute\QueryHandler;
+use Spiral\Cqrs\QueryBusInterface;
 
 final class FindEventsHandler extends EventsHandler
 {
     public function __construct(
         private readonly EventRepositoryInterface $events,
-    ) {}
+        QueryBusInterface $bus,
+    ) {
+        parent::__construct($bus);
+    }
 
     #[QueryHandler]
     public function __invoke(FindEvents $query): iterable
     {
         return $this->events->findAll(
-            scope: self::getScopeFromFindEvents($query),
+            scope: $this->getScopeFromFindEvents($query),
             orderBy: ['timestamp' => 'desc'],
             limit: $query->limit,
         );
