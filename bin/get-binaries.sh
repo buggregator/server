56b6614917872238b1bbf8fd12ab55158707b09f
Pavel Buchnev <butschster@gmail.com>
2024-08-13 10:21:54 +0400

    Reverted Dolt DB back to the previous stable version `1.42.8`. This fixes the "There is already an active transaction" error in `MySQLDriver.php` that started appearing after upgrading to 1.42.9.
    fixes #237

--- bin/get-binaries.sh
+++ bin/get-binaries.sh
@@ -5,7 +5,7 @@ rm -rf centrifugo_4.0.3_linux_amd64.tar.gz
 chmod +x centrifugo
 
 echo "Download dolt db"
-wget --timeout=10 https://github.com/dolthub/dolt/releases/download/v1.35.12/dolt-linux-amd64.tar.gz
+wget --timeout=10 https://github.com/dolthub/dolt/releases/download/v1.42.8/dolt-linux-amd64.tar.gz
 tar xvfz dolt-linux-amd64.tar.gz dolt-linux-amd64/bin/dolt
 rm -rf dolt-linux-amd64.tar.gz
 mv dolt-linux-amd64/bin/dolt dolt
